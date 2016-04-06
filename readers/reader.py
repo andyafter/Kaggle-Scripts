b9bb586e79e645a1913dc036a4ee747286d55426
andyafter <andyafter@gmail.com>
2016-04-06 16:14:56 +0800

    Modified code for quality.

--- readers/reader.py
+++ readers/reader.py
@@ -1,13 +1,5 @@
-import os
-from datetime import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-from time import time
-import seaborn as sns
 import pandas as pd
-import scipy
 import zipfile
-from matplotlib.backends.backend_pdf import PdfPages
 
 
 class DataReader():

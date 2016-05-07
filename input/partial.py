41980a572ae45af8f4797e621f5dfa1d3ab0a06e
andyafter <andyafter@gmail.com>
2016-05-07 10:42:13 +0800

    Added data dependencies.

--- input/partial.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import zipfile
-
-drive = './'
-z = zipfile.ZipFile(drive+'train.csv.zip')
-train = pd.read_csv(z.open('train.csv'))
-cats = list(set(train.Category))
-print cats

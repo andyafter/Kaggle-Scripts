ac60b07a0b8cb5e31725dfe0d145d19642366cf7
andyafter <andyafter@gmail.com>
2016-05-18 23:12:16 +0800

    Added convolution script.

---
+++ main/satellite/edges.py
@@ -0,0 +1,5 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+
+# Mainly for convolutional stuff here

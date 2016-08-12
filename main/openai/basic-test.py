8eb35a888fc65d35671b77d9ec1db6546b7984fa
andyafter <andyafter@gmail.com>
2016-08-13 00:43:22 +0800

    Added openAI simple gym test

---
+++ main/openai/basic-test.py
@@ -0,0 +1,17 @@
+# in order to run this script you need to install openAI gym:
+# git clone https://github.com/openai/gym
+# cd gym
+# sudo pip install -e . # minimal install
+
+import gym
+env = gym.make('CartPole-v0')
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break

af2a7adaed069b330ca0e3a784fc780968c91cc5
VR_room <VR_room>
2016-08-16 15:14:28 +0800

    Changed doc names

---
+++ main/digital_recognizer/reader.py
@@ -0,0 +1,9 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
+
+
+class DataReader():
+    def __init__(self):
+        self.drive = '../../input/digitalrecognizer/'
+        
